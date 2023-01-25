3245f5b8a60bbc22b0a4f8cb9edf553599bf7ac6
Giuliana Paola Diaz Luna <giulygael2018@gmail.com>
2023-01-25 12:28:42 -0300

    Funciones en Python

---
+++ Python/Leccion 4/Ejercicio6.py
@@ -0,0 +1,19 @@
+# Ejercicio 6: Tabla de multiplicar
+# Hacer un programa que pida un número por teclado y guarde
+# en una lista su tabla de multiplicar hasta el 10. Por ejemplo:
+# Si digita el 5 la lista tendrá: 5,10,15,20,25,30,35,40,45,50
+
+numero = int(input("Digite un número: "))
+lista = [] # Creamos una lista vacia
+for i in range(1, 11):
+    lista.append(i*numero) # llenamos la lista y vamos a ir guardando el iterador que esta recorriendo multiplicado por el numero digitado por el usuario
+
+print(f'\nTabla de multiplicar del {numero}: \n {lista}')
+
+for i in range(1,11):
+    print(f'\n {numero} x {i} = {i*numero}')
+
+
+#for indice,i in enumerate(lista):
+    #print(f'\n {numero} x {i} = {lista[indice]}') # Este ciclo es para ver el formato de una tabla de multiplicar
+
---
+++ Python/Leccion 5/Ejercicio01.py
@@ -0,0 +1,17 @@
+# Ejercicio 01: Crear una función para sumar los valores recibidos de tipo
+# numéricos, utilizando argumentos variables *args como parametro de la
+# Función y agregar como resultado la suma de todos los valores pasados
+# como argumentos.
+
+# Definimos una función
+def sumar_valor(*args):  # Recibimos una cantidad de parámetros indefinidos
+    # pass sirve para seguir programando y luego volver a la fx
+    resultado = 0
+    # Iteramos cada elemento
+    for valor in  args:
+        resultado += valor
+    return resultado
+
+
+# Llamamos a la función
+print(sumar_valor(3, 5, 9, 2, 1))
