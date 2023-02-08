7e0b3b7838636d61ae14e742fc9d8ded6d1f2c82
Giuliana Paola Diaz Luna <giulygael2018@gmail.com>
2023-02-08 11:05:17 -0300

    Clases abstractas

--- Python/Leccion 9/FiguraGeometrica.py
+++ Python/Leccion 9/FiguraGeometrica.py
@@ -3,14 +3,14 @@ from abc import ABC, abstractmethod
 
 # ABC significa: Abstract Base Class, convierte una clase en abstracta
 
-class FiguraGeometrica(ABC):
+class FiguraGeometrica(ABC): # el parametro ABC indica que ahora la clase Figura Geometrica es hija de la clase abstracta ABC
     def __init__(self, ancho, alto):
-        if self._validar_valores(ancho):
+        if self._validar_valores(ancho): # equivale a if 0 < ancho < 10
             self._ancho = ancho
         else:
             self._ancho = 0
             print(f'Valor erroneo para el ancho: {ancho}')
-        if self._validar_valores(alto):
+        if self._validar_valores(alto): # equivale a if 0 < alto < 10
             self._alto = alto
         else:
             self._alto = 0
@@ -22,7 +22,7 @@ class FiguraGeometrica(ABC):
 
     @ancho.setter
     def ancho(self, ancho):
-        if self._validar_valores(ancho):
+        if self._validar_valores(ancho): # validamos el metodos sett
             self._ancho = ancho
         else:
             print(f'valor erroneo ancho: {ancho}')
@@ -38,12 +38,13 @@ class FiguraGeometrica(ABC):
         else:
             print(f'valor erroneo alto: {alto}')
 
-    @abstractmethod
-    def calcular_area(self):
+    # agregamos el metodo abstracto debajo del ultimo sett y arriba dem metodo str
+    @abstractmethod # decorador
+    def calcular_area(self): # el metodo calcular area es abtracto pero no se implementa, obliga a las clases hijas que implemente el metodo calcular_area
         pass
 
     def __str__(self):
         return f'FiguraGeometrica [Ancho: {self._ancho}, Alto: {self._alto}]'
 
-    def _validar_valores(self, valor):  # Método encapsulado
-        return True if 0 < valor < 10 else False
+    def _validar_valores(self, valor):  # Método encapsulado con guion bajo
+        return True if 0 < valor < 10 else False # sintaxis simplificada
--- Python/Leccion 9/test_figura_geometrica.py
+++ Python/Leccion 9/test_figura_geometrica.py
@@ -6,10 +6,11 @@ from Rectangulo import Rectangulo
 
 print('Creación de objeto clase Cuadrado'.center(50, '_'))
 cuadrado1 = Cuadrado(8, 'Azul') # le pasamos los argumentos
-cuadrado1.alto = 7
+# Reasignacion de valores
+cuadrado1.alto = -7
 # cuadrado1.ancho = 7
-# print(cuadrado1.ancho)
-# print(cuadrado1.alto)
+print(cuadrado1.ancho) # muestra el valor aceptado
+print(cuadrado1.alto)
 print(f'Cálculo del área del cuadrado: {cuadrado1.calcular_area()}')
 
 # MRO = Method Resolution Order
@@ -20,9 +21,12 @@ print(cuadrado1) # aqui muestra el metodo dunder str de la clase cuadrada
 
 print('Creación de objeto clase Rectangulo'.center(50, '_'))
 rectangulo1 = Rectangulo(3, 9, 'verde')
+
+# reasignacion de valores
 rectangulo1.ancho = 8
 print(f'Calculo del area del rectangulo: {rectangulo1.calcular_area()}')
 print(rectangulo1) # aqui nos muestra el metodo str
 
-# figura1 = FiguraGeometrica() No se puede instanciar, es abstracta
+# Clase Figura Geometrica como clase abstracta
+# figura1 = FiguraGeometrica() Da error, No se puede instanciar, es abstracta
 print(Cuadrado.mro())
